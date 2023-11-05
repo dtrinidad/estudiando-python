4bbf0fc50a06d6379500c00834a7f6c35d483095
dtrinidad <ing.dtrinidad@gmail.com>
2023-11-05 13:56:15 +0000

    added video 18

---
+++ PildorasInformaticas/17.-BuclesWhile.py
@@ -0,0 +1,22 @@
+import math
+
+print ("programa de calculo de raiz cuadrada")
+numero=int(input("Introduce un numero por favor: "))
+
+intentos=0
+
+while numero < 0:
+    print("No se puede hallar la raiz de un numero negativo!")
+    if intentos == 2:
+        print("Has consumido el numero maximo de intentos. \
+              El programa ha filalizado")
+        break
+
+    numero=int(input("Introduce un numero por favor: "))
+    if numero < 0:
+        intentos = intentos + 1
+        print(intentos)
+
+if intentos < 2:
+    solucion=math.sqrt(numero)
+    print(f"La raiz cuadrada de {str(numero)} es {str(solucion)}")
---
+++ PildorasInformaticas/18.-Continue-Pass-Else.py
@@ -0,0 +1,17 @@
+#continue
+'''
+  esto lo  que hace es pasar a la siguiente iteracion del bucle a partir 
+  de la lectura de esta instruccion
+'''
+
+#pass
+'''
+  devolver null en cuanto se lee dentro del bucle.
+  es poco usado, se suele utilizar a la hora de marcar una clase/bucle que aun 
+  no la has desarrollado.
+'''
+
+#else
+'''
+es similar a como funciona en un condicionar
+'''
